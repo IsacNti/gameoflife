9e17f1132b57a60c70cfb31a057a95ebf68b2696
IsacNti <77719323+IsacNti@users.noreply.github.com>
2022-12-16 13:39:17 +0100

    första lektion

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -2,15 +2,35 @@ package Model;
 import View.*;
 
 public class Model {
+
+    public int granne = 0;
+
     public Model(int width, int height) {
 
     }
 
     public void update() {
+    GetRules();
+    }
+
+    private void GetRules() {
+
+
+
+
+
+        if (granne == 2 || granne== 3){
+
+
+        }
+            else{
+
+        }
+
     }
 
     public Shape[] getShapes() {
-        Point[] points = {new Point(5,5)};
+        Point[] points = {new Point(5,5),new Point(6,5),new Point(7,5)};
         return (Shape[])points;
     }
 }
